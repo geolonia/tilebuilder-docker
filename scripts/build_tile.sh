41c0a5134c8ac6e21493341f788b8be366eaa009
naoki <n.globe.us@gmail.com>
2025-04-09 12:05:13 +0900

    .prj を外部から指定するように修正

--- scripts/build_tile.sh
+++ scripts/build_tile.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-PRJ_CONTENT='GEOGCS["GCS_JGD_2000",DATUM["D_JGD_2000",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]'
-
 # デフォルトディレクトリ
 TARGET_DIR="/data"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -21,12 +19,6 @@ find "$TARGET_DIR" -iname "*.shp" | while read -r shpfile; do
     if [ ! -f "$cpg_file" ]; then
         echo "UTF-8" > "$cpg_file"
         echo "Created .cpg: $cpg_file"
-    fi
-
-    prj_file="${shpfile%.shp}.prj"
-    if [ ! -f "$prj_file" ]; then
-        echo "$PRJ_CONTENT" > "$prj_file"
-        echo "Created .prj: $prj_file"
     fi
 
     tmp_ndjson="/tmp/${base}.ndjson"
