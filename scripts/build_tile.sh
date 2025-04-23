6575a838187a21e808dee9bafb1e89110894b573
naoki <n.globe.us@gmail.com>
2025-04-23 09:34:54 +0900

    出力したMBTilesを1つに結合

--- scripts/build_tile.sh
+++ scripts/build_tile.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 
 CONFIG_FILE="kata.yml"
 OUTPUT_DIR="/data/tiles"
 mkdir -p "$OUTPUT_DIR"
 
+# ▼ tiles/ 以下に .mbtiles が存在するかチェック
+existing_mbtiles=($(find tiles/ -type f -name "*.mbtiles"))
+rm -f "${existing_mbtiles[@]}"
+
 # 全体 default 設定を取得
 default_cpg=$(yq e '.default.cpg // "UTF-8"' "$CONFIG_FILE")
 default_prj=$(yq e '.default.prj // "EPSG:4326"' "$CONFIG_FILE") # 入力側の指定に使う
+merge_minzoom=$(yq e '.default.minzoom // 8' "$CONFIG_FILE")
+merge_maxzoom=$(yq e '.default.maxzoom // 14' "$CONFIG_FILE")
 
 # default を除いたレイヤー一覧
 source_layers=$(yq e 'keys | .[]' "$CONFIG_FILE" | grep -v '^default$')
@@ -48,6 +54,21 @@ for source_layer in $source_layers; do
     -z "$maxzoom" -Z "$minzoom" \
     --drop-densest-as-needed \
     "${tmp_ndjson_list[@]}"
+
+  # 生成した .mbtiles をマージ
+  TILEJOIN_OPTS=(
+      "--overzoom"
+      "--no-tile-size-limit"
+      "-Z" "$merge_minzoom"
+      "-z" "$merge_maxzoom"
+      "--force"
+  )
+
+  IFS=$'\n' #スペースを含むファイル名に対応
+  mbtiles_files=($(find . -name "*.mbtiles" ! -name "output.mbtiles"))
+  echo "マージする .mbtiles ファイル: ${mbtiles_files[@]}"
+  merged_file="${OUTPUT_DIR}/output.mbtiles"
+  tile-join -o "$merged_file" "${TILEJOIN_OPTS[@]}" "${mbtiles_files[@]}"
 
   # クリーンアップ
   rm -f "${tmp_ndjson_list[@]}"
