8a1ad9c78540a926d2b7b5fb648a0625add43299
naoki <n.globe.us@gmail.com>
2025-05-01 13:44:37 +0900

    処理を修正

--- scripts/build_tile.sh
+++ scripts/build_tile.sh
@@ -5,6 +5,9 @@ CONFIG_FILE="/data/kata.yml"
 OUTPUT_DIR="/data/tiles"
 mkdir -p "$OUTPUT_DIR"
 
+# 処理の開始時間を記録
+start_time=$(date +%s)
+
 # 全体 default 設定を取得
 default_cpg=$(yq e '.default.cpg // "UTF-8"' "$CONFIG_FILE")
 default_prj=$(yq e '.default.prj // "EPSG:4326"' "$CONFIG_FILE") # 入力側の指定に使う
@@ -24,6 +27,9 @@ for source_layer in $source_layers; do
   if [ -f "$mbtiles_file" ]; then
     echo "MBTiles がすでに存在しているので: $mbtiles_file の生成をスキップします"
   else
+
+    # 個別のタイルを作るのにかかった時間を記録
+    start_time_layer=$(date +%s)
 
     minzoom=$(yq e ".\"$source_layer\".minzoom" "$CONFIG_FILE")
     maxzoom=$(yq e ".\"$source_layer\".maxzoom" "$CONFIG_FILE")
@@ -54,6 +60,11 @@ for source_layer in $source_layers; do
       -z "$maxzoom" -Z "$minzoom" \
       --drop-densest-as-needed \
       "${tmp_ndjson_list[@]}"
+
+    # タイル生成にかかった時間を記録
+    end_time_layer=$(date +%s)
+    elapsed_time_layer=$((end_time_layer - start_time_layer))
+    echo "  → $source_layer.mbtiles の生成にかかった時間: $((elapsed_time_layer / 60)) 分 $((elapsed_time_layer % 60)) 秒"
   fi
 done
 
@@ -75,4 +86,9 @@ tile-join -o "$merged_file" "${TILEJOIN_OPTS[@]}" "${mbtiles_files[@]}"
 # クリーンアップ
 rm -f "${tmp_ndjson_list[@]}"
 echo "全てのタイルの生成が完了しました: $mbtiles_file"
-echo ""
+
+# 処理の終了時間を記録
+end_time=$(date +%s)
+elapsed_time=$((end_time - start_time))
+echo "全てのタイル生成にかかった時間: $((elapsed_time / 60)) 分 $((elapsed_time % 60)) 秒"
+echo "タイル生成を終了します: $(date)"
